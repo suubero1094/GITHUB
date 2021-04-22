27213420451c42172565dda427df0c0ec6af63a6
suube <suube@DESKTOP-BGL2FIL>
2021-04-22 18:08:24 +0200

    CONFLICTO

--- src/github/SEGUNDOJAVA.java
+++ src/github/SEGUNDOJAVA.java
@@ -2,11 +2,9 @@ package github;
 
 public class SEGUNDOJAVA {
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
      System.out.println("cambio linea 5 eclipse");
-		}
-=======
-			System.out.println("Linea 5 modificada en Github");	
+     System.out.println("Linea 5 modificada en Github");	
 	}
->>>>>>> branch 'master' of https://github.com/suubero1094/GITHUB.git
+
 }
